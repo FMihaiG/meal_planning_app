9b3ab8366ea0b4c6e30691853c9d78c4c286c8c4
FMihaiG <98691101+FMihaiG@users.noreply.github.com>
2022-02-06 14:01:06 +0100

    Update app.R

--- app.R
+++ app.R
@@ -16,8 +16,8 @@ rm(list=ls())
 path_storage = "/srv/shiny-server/meal_planning_app/Local storage/"
 
 # Source ui and server
-source('C:/R_work/Meal_app_PROD/myUI.R', local = TRUE)
-source('C:/R_work/Meal_app_PROD/myServer.R', local = TRUE)
+source('/srv/shiny-server/meal_planning_app/myUI.R', local = TRUE)
+source('/srv/shiny-server/meal_planning_app/myServer.R', local = TRUE)
 
 shinyApp(ui = myUI, server = myServer)
 
